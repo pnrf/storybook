2fc11429488fcc37a1e898b8aa0fdff1dabc95da
Pankratov <pnrf@yandex.ru>
2023-04-23 12:12:51 +0300

    feat: add page Logo

---
+++ src/stories/kids-story/logo/logo.jsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+  return (
+    <img src='https://github.com/pnrf/storybook/blob/main/src/stories/kids-story/logo/assets/logo.svg' alt="Пример отображения логотипа сайта в Header" />
+  )
+}
